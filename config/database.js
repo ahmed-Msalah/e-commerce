3d05bef0e07243069d673fc9bc3564b83a80e190
Ahmed <ahmedmohammedsalah200@gmail.com>
2024-12-09 23:51:51 +0200

    vercel added

--- config/database.js
+++ config/database.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
-
+const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const dbConnection = () => {
-  mongoose
-    .connect(process.env.DB_URI)
-    .then((conn) => {
-      console.log(`Database Connected: ${conn.connection.host}`);
-    })
-    // .catch((err) => {
-    //   console.error(`Database Error: ${err}`);
-    //   process.exit(1);
-    // });
+  mongoose.connect(db).then((conn) => {
+    console.log(`Database Connected: ${conn.connection.host}`);
+  });
+  // .catch((err) => {
+  //   console.error(`Database Error: ${err}`);
+  //   process.exit(1);
+  // });
 };
 
 module.exports = dbConnection;
